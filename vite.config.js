e474297562b20dea663753aead4b31b894c8dfcc
alejdan <alejdan@gmail.com>
2025-11-19 15:08:37 -0600

    added vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/space-shooter-game/',
   server: {
     host: '127.0.0.1',
     port: 3000,
